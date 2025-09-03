f1a80847bc039dd1497c73d44b477b33b0a5526d
Todd Riley <todd@websbytodd.com>
2025-09-03 15:12:43 -0400

    Organize the code a bit.

--- browser-extension/src/entrypoints/content.ts
+++ browser-extension/src/entrypoints/content.ts
@@ -2,19 +2,28 @@ import OverType from "../overtype/overtype";
 
 export default defineContentScript({
   main() {
-    const ghCommentBox = document.getElementById("new_comment_field")!;
-    ghCommentBox.classList.add("overtype-input");
-    const previewDiv = document.createElement("div");
-    previewDiv.classList.add("overtype-preview");
-    ghCommentBox.insertAdjacentElement("afterend", previewDiv);
-    const ghCommentWrapper = ghCommentBox.parentElement!.closest("div")!;
-    ghCommentWrapper.classList.add("overtype-wrapper");
-    const ghCommentContainer = ghCommentWrapper.parentElement!.closest("div")!;
-    ghCommentContainer.classList.add("overtype-container");
-    new OverType(ghCommentContainer.parentElement!.closest("div")!, {
-      placeholder: "Add your comment here...",
-    });
+    const ghCommentBox = document.getElementById("new_comment_field") as
+      | HTMLTextAreaElement
+      | undefined;
+    if (ghCommentBox) {
+      const overtypeContainer = modifyGithubDOM(ghCommentBox);
+      new OverType(overtypeContainer, {
+        placeholder: "Add your comment here...",
+      });
+    }
   },
   matches: ["<all_urls>"],
   runAt: "document_end",
 });
+
+function modifyGithubDOM(ghCommentBox: HTMLTextAreaElement) {
+  ghCommentBox.classList.add("overtype-input");
+  const previewDiv = document.createElement("div");
+  previewDiv.classList.add("overtype-preview");
+  ghCommentBox.insertAdjacentElement("afterend", previewDiv);
+  const ghCommentWrapper = ghCommentBox.parentElement!.closest("div")!;
+  ghCommentWrapper.classList.add("overtype-wrapper");
+  const ghCommentContainer = ghCommentWrapper.parentElement!.closest("div")!;
+  ghCommentContainer.classList.add("overtype-container");
+  return ghCommentContainer.parentElement!.closest("div")!;
+}
