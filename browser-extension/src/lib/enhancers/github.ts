b28f23caa1a3430b6c116592e10ddba05fba8c68
Ned Twigg <ned.twigg@diffplug.com>
2025-09-04 14:34:00 -0700

    Fix the regex so it works on issues too (breaks the issues CSS)

--- browser-extension/src/lib/enhancers/github.ts
+++ browser-extension/src/lib/enhancers/github.ts
@@ -5,10 +5,10 @@ import type { CommentEnhancer, CommentSpot } from '../enhancer'
 
 const GITHUB_SPOT_TYPES = [
   'GH_PR_ADD_COMMENT',
+  'GH_ISSUE_ADD_COMMENT',
   /* TODO
   'GH_ISSUE_NEW',
   'GH_PR_NEW',
-  'GH_ISSUE_ADD_COMMENT',
   'GH_ISSUE_EDIT_COMMENT',
   'GH_PR_EDIT_COMMENT',
   'GH_PR_CODE_COMMENT',
@@ -38,10 +38,11 @@ export class GitHubAddCommentEnhancer implements CommentEnhancer<GitHubAddCommen
     // Parse GitHub URL structure: /owner/repo/issues/123 or /owner/repo/pull/456
     logger.debug(`${this.constructor.name} examing url`, window.location.pathname)
 
-    const match = window.location.pathname.match(/^\/([^/]+)\/([^/]+)(?:\/pull\/(\d+))/)
+    const match = window.location.pathname.match(/^\/([^/]+)\/([^/]+)(?:\/(pull|issues)\/(\d+))/)
     logger.debug(`${this.constructor.name} found match`, window.location.pathname)
     if (!match) return null
-    const [, owner, repo, numberStr] = match
+    const [, owner, repo, issuesOrPull, numberStr] = match
+    const type = issuesOrPull === 'issues' ? 'GH_ISSUE_ADD_COMMENT' : 'GH_PR_ADD_COMMENT'
     const slug = `${owner}/${repo}`
     const number = parseInt(numberStr!, 10)
 
@@ -51,7 +52,7 @@ export class GitHubAddCommentEnhancer implements CommentEnhancer<GitHubAddCommen
       domain: 'github.com',
       number,
       slug,
-      type: 'GH_PR_ADD_COMMENT',
+      type,
       unique_key,
     }
     return [this.createOvertypeFor(textarea), spot]
