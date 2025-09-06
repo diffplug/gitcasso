48c389ddd580cb401412bf078207b6459f18211f
Ned Twigg <ned.twigg@diffplug.com>
2025-09-05 22:15:52 -0700

    We're not running coverage at all right now.

--- browser-extension/vitest.config.ts
+++ browser-extension/vitest.config.ts
@@ -4,18 +4,6 @@ import { WxtVitest } from 'wxt/testing'
 export default defineConfig({
   plugins: [WxtVitest()],
   test: {
-    coverage: {
-      exclude: [
-        'entrypoints/**/*.d.ts',
-        'entrypoints/**/types.ts',
-        'entrypoints/**/config.ts',
-        'node_modules',
-        'tests',
-      ],
-      include: ['entrypoints/**/*.ts'],
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-    },
     environment: 'node',
     pool: 'threads',
     globals: true,
