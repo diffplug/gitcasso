c338684bee397e7e6edf0b51c2ee1d8194b8b1a8
Ned Twigg <ned.twigg@diffplug.com>
2025-09-06 00:32:48 -0700

    Add a test for issues too.

--- browser-extension/tests/lib/enhancers/github.test.ts
+++ browser-extension/tests/lib/enhancers/github.test.ts
@@ -6,7 +6,7 @@ expect
 import { EnhancerRegistry } from '../../../src/lib/registries'
 
 describe('github', () => {
-  usingHar('gh_pr').it('should identify gh_pr textarea and create proper spot object', async () => {
+  usingHar('gh_pr').it('should create the correct spot object', async () => {
     const enhancers = new EnhancerRegistry()
     const textareas = document.querySelectorAll('textarea')
     expect(textareas.length).toBe(2)
@@ -18,6 +18,20 @@ describe('github', () => {
         "slug": "diffplug/selfie",
         "type": "GH_PR_ADD_COMMENT",
         "unique_key": "github.com:diffplug/selfie:517",
+      }
+    `)
+  })
+  usingHar('gh_issue').it('should create the correct spot object', async () => {
+    const enhancers = new EnhancerRegistry()
+    const textareas = document.querySelectorAll('textarea')
+    expect(textareas.length).toBe(1)
+    expect(enhancers.tryToEnhance(textareas[0]!)?.spot).toMatchInlineSnapshot(`
+      {
+        "domain": "github.com",
+        "number": 523,
+        "slug": "diffplug/selfie",
+        "type": "GH_ISSUE_ADD_COMMENT",
+        "unique_key": "github.com:diffplug/selfie:523",
       }
     `)
   })
