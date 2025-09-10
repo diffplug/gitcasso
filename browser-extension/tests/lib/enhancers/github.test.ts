97948e7a800f0904f31eb81574a3ef45bfe5fc14
Todd Riley <todd@websbytodd.com>
2025-09-10 14:58:50 -0400

    tests

--- browser-extension/tests/lib/enhancers/github.test.ts
+++ browser-extension/tests/lib/enhancers/github.test.ts
@@ -6,7 +6,7 @@ expect
 import { EnhancerRegistry } from '../../../src/lib/registries'
 
 describe('github', () => {
-  usingHar('gh_pr').it('should create the correct spot object', async () => {
+  usingHar('gh_pr').it('should create the correct spot object for gh_pr', async () => {
     const enhancers = new EnhancerRegistry()
     const textareas = document.querySelectorAll('textarea')
     expect(textareas.length).toBe(2)
@@ -21,7 +21,20 @@ describe('github', () => {
       }
     `)
   })
-  usingHar('gh_issue').it('should create the correct spot object', async () => {
+  usingHar('gh_new_pr').it('should create the correct spot object for gh_new_pr', async () => {
+    const enhancers = new EnhancerRegistry()
+    const textareas = document.querySelectorAll('textarea')
+    expect(textareas.length).toBe(2)
+    expect(enhancers.tryToEnhance(textareas[0]!)?.spot).toMatchInlineSnapshot(`
+          {
+            "domain": "github.com",
+            "slug": "diffplug/selfie/main...cavia-porcellus:selfie:main",
+            "type": "GH_PR_NEW_COMMENT",
+            "unique_key": "github.com:diffplug/selfie/main...cavia-porcellus:selfie:main",
+          }
+        `)
+  })
+  usingHar('gh_issue').it('should create the correct spot object for gh_issue', async () => {
     const enhancers = new EnhancerRegistry()
     const textareas = document.querySelectorAll('textarea')
     expect(textareas.length).toBe(1)
@@ -34,5 +47,20 @@ describe('github', () => {
         "unique_key": "github.com:diffplug/selfie:523",
       }
     `)
+  })
+  usingHar(
+    'gh_new_issue',
+  ).it('should create the correct spot object for gh_new_issue', async () => {
+    const enhancers = new EnhancerRegistry()
+    const textareas = document.querySelectorAll('textarea')
+    expect(textareas.length).toBe(1)
+    expect(enhancers.tryToEnhance(textareas[0]!)?.spot).toMatchInlineSnapshot(`
+          {
+            "domain": "github.com",
+            "slug": "diffplug/selfie",
+            "type": "GH_ISSUE_NEW_COMMENT",
+            "unique_key": "github.com:diffplug/selfie:new",
+          }
+        `)
   })
 })
