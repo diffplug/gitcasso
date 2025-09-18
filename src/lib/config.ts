994911d3e4e4139ed1ec42d10ef444a18969a63b
Ned Twigg <ned.twigg@diffplug.com>
2025-09-18 02:35:31 -0700

    Remove the long-dead cyan trick.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,7 +7,6 @@ const LOG_LEVELS = ['DEBUG', 'INFO', 'WARN', 'ERROR'] as const
 export type LogLevel = (typeof LOG_LEVELS)[number]
 
 export const CONFIG = {
-  ADDED_OVERTYPE_CLASS: 'gitcasso-overtype',
   EXTENSION_NAME: 'gitcasso', // decorates logs
   LOG_LEVEL: 'DEBUG' satisfies LogLevel,
   MODE: 'PROD' satisfies ModeType,
