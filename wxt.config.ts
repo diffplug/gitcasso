f2f57110235c5292fdcfdbb38e7ea6835220f0ee
Ned Twigg <ned.twigg@diffplug.com>
2025-09-26 14:12:19 -0700

    Start off with permissions on `github.com` only.

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,16 +6,17 @@ import { defineConfig } from 'wxt'
 export default defineConfig({
   manifest: {
     description:
-      'Syntax highlighting and autosave for comments on GitHub (and other other markdown-friendly websites).',
-    host_permissions: ['https://*/*', 'http://*/*'],
+      'Syntax highlighting and autosave for comments on GitHub (and other markdown-friendly websites).',
+    host_permissions: ['https://github.com/*'],
     icons: {
       16: '/icons/icon-16.png',
       48: '/icons/icon-48.png',
       128: '/icons/icon-128.png',
     },
     name: 'Gitcasso',
+    optional_host_permissions: ['https://*/*', 'http://*/*'],
     permissions: ['activeTab', 'tabs'],
-    version: '1.0.0',
+    version: '0.1.0',
   },
   modules: ['@wxt-dev/webextension-polyfill'],
   srcDir: 'src',
