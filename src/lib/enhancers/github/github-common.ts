aaca3ffd5a5ceaedfb660f6385f7b11a7415e43f
Ned Twigg <ned.twigg@diffplug.com>
2025-10-01 15:57:31 -0700

    Centralize our github-centered color scheme into github-common.

--- src/lib/enhancers/github/github-common.ts
+++ src/lib/enhancers/github/github-common.ts
@@ -11,6 +11,27 @@ export const commonGitHubOptions: Options = {
 
 export function prepareGitHubHighlighter() {
   oncePerRefresh("github-highlighter", () => {
+    const textColor = "rgb(31, 35, 40)"
+    const headingColor = "rgb(174, 52, 151)"
+    OverType.setTheme({
+      colors: {
+        blockquote: "rgb(89, 99, 110)",
+        code: "#59636e",
+        codeBg: "#f6f8fa",
+        cursor: "#000000",
+        em: "rgb(126, 123, 255)",
+        h1: headingColor,
+        h2: headingColor,
+        h3: headingColor,
+        hr: "#5a7a9b",
+        link: "rgb(9, 105, 218)",
+        selection: "rgba(0, 123, 255, 0.3)",
+        strong: "rgb(45, 1, 142)",
+        syntaxMarker: textColor,
+        text: textColor,
+      },
+      name: "custom-github",
+    })
     OverType.setCodeHighlighter(githubHighlighter)
   })
 }
